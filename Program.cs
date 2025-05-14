e6ff61ef10ed4e1d12c00f7f69cc4a8b32583d2d
MotiWolff <motiwolff@gmail.com>
2025-05-14 15:41:48 +0300

    Deleting old content

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
-﻿using System.Threading.Tasks;
-
-namespace Git
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello people!");
-            Console.WriteLine("This was printed by the other branch!");
-        }
-    }
-}
+﻿
