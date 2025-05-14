784aa9ee3e0441966d39f10e35387755924ca620
ahronSteinbruch <139044285+ahronSteinbruch@users.noreply.github.com>
2025-05-14 17:18:19 +0300

    add the class AtbashDecrypter with func Decryp

---
+++ AtbashDecrypter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class AtbashDecrypter
+{
+    private static Dictionary<char, char> azcy = new Dictionary<char, char>();
+
+    //dict thet hold a-z b-y cipher
+    static AtbashDecrypter()
+    {
+        for(char c = 'A'; c <= 'Z'; c++) azcy[c] = (char)('Z' - (c - 'A'));
+        for(char c = 'a'; c <= 'z'; c++) azcy[c] = (char)('z' -  (c - 'a'));
+    }
+
+    // Decrypt the data if data use a-z b-y cipher
+    public static string Decrypt(string[] input)
+    {
+        string[] decrypted = new string[string.Length];
+
+        for(int i = 0;i < input.Length;i++)
+        {
+            string in = "";
+            foreach(char c in input)
+            {
+                input += azcy.ContainsKey(c) ? azcy[c] : c;
+            }
+            decrypted[i] = in;
+        }
+        return decrypted;
+    }
+
+
+
+
+}
